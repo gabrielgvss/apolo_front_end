f74cdd46ab4f390d07595c1cc258727e736abdc9
Kauã Nogueira <81389414+kauaumnougueira@users.noreply.github.com>
2023-10-18 17:08:25 -0300

    subir

---
+++ assets/js/spa.js
@@ -0,0 +1,23 @@
+function render(div, component, scriptsrc){
+    //fetch de render
+    fetch(component)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Erro ao carregar o componente: ${response.status}`);
+            }
+            return response.text()
+        })  
+        .then(html => {
+            div.innerHTML = html
+            if(scriptsrc !== null){
+                const script = document.createElement('script');
+                script.type = 'module'
+                script.src = scriptsrc;
+                script.onload = function() {
+                    console.log('Script carregado!');
+                };
+                div.appendChild(script)
+            }
+        })
+        .catch(error => console.error(error));
+}   
